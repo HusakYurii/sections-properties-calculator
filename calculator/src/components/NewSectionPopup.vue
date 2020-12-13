2a5bfd503128712fee942b0830dbe16a1d3cd132
Husak Yurii <gusak.1881@gmail.com>
2020-12-13 20:07:15 +0100

    add a new component to show when a new section is about to be added

---
+++ calculator/src/components/NewSectionPopup.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="overlay" class="hidden">
+    <div id="new-section">
+      <button id="close-button" @click.stop="$emit(`closePopup`)">x</button>
+      <button id="confirm-button" @click.stop="$emit(`addSection`)">
+        Add Section
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export interface SectionPopupInterface {
+  hide: () => void;
+  show: () => void;
+}
+
+export default Vue.extend({
+  name: "NewSectionPopup",
+  methods: {
+    hide(): void {
+      this.$el.setAttribute("class", "hidden");
+    },
+    show(): void {
+      this.$el.setAttribute("class", "");
+    },
+  },
+});
+</script>
+
+<style>
+#overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(36, 4, 180, 0.294);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#overlay.hidden {
+  visibility: hidden;
+}
+
+/* The window where properties will be chosen for a new section */
+
+#new-section {
+  height: 600px;
+  width: 800px;
+  background-color: #019ad2;
+  border-radius: 10px;
+  position: relative;
+}
+
+/* Button common styles */
+#confirm-button,
+#close-button {
+  cursor: pointer;
+  color: #ffffff;
+  font-size: 15px;
+  text-align: center;
+  line-height: 15px;
+  padding: 5px;
+}
+
+/* Close button */
+#close-button {
+  background-color: #e4685d;
+  border-radius: 20px;
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+}
+
+#close-button:hover {
+  background-color: #eb675e;
+}
+
+#close-button:focus {
+  outline: none;
+}
+
+/* Confirm to add a new section button */
+#confirm-button {
+  background-color: #44c767;
+  border-radius: 5px;
+  width: 100px;
+  height: 30px;
+  cursor: pointer;
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+#confirm-button:hover {
+  background-color: #5cbf2a;
+}
+
+#confirm-button:focus {
+  outline: none;
+}
+</style>
